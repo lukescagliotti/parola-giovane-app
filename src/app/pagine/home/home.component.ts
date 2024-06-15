afb4b7af2cdbc9048633f0ebbbef4139e8db4644
luca <luca.scagliotti@gruppoisei.it>
2024-06-15 19:03:02 +0200

    add short list of next 3 events into home

--- src/app/pagine/home/home.component.ts
+++ src/app/pagine/home/home.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+interface Evento {
+  data: string;
+  evento: string;
+}
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  imports: [CommonModule, HttpClientModule]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  eventi: Evento[] = [];
+  prossimiEventi: Evento[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Evento[]>('assets/eventi.json').subscribe(data => {
+      this.eventi = data;
+      this.prossimiEventi = this.getProssimiEventi(this.eventi);
+    });
+  }
+
+  getProssimiEventi(eventi: Evento[]): Evento[] {
+    const oggi = new Date();
+    return eventi
+      .filter(evento => new Date(evento.data) >= oggi)
+      .sort((a, b) => new Date(a.data).getTime() - new Date(b.data).getTime())
+      .slice(0, 3);
+  }
 }
