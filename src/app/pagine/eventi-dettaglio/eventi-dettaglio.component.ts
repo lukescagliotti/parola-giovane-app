8e2706c402e71e3a173e541c28b1a4cf855b920b
luca <luca.scagliotti@gruppoisei.it>
2024-08-26 17:00:09 +0200

    fix per salvataggio a calendario e eventi a giornata intera

--- src/app/pagine/eventi-dettaglio/eventi-dettaglio.component.ts
+++ src/app/pagine/eventi-dettaglio/eventi-dettaglio.component.ts
@@ -29,9 +29,18 @@ export class EventiDettaglioComponent implements OnInit, AfterViewInit {
     const firstEvent = this.data.eventi[0];
     const eventDate = new Date(firstEvent.data);
     this.giorno = eventDate.getDate();
-    this.mese = eventDate.toLocaleString('it-IT', { month: 'long' }); // Converte il mese in una stringa leggibile in italiano
+    this.mese = eventDate.toLocaleString('it-IT', { month: 'long' });
     this.anno = eventDate.getFullYear();
+
+    // Creazione manuale delle ore per eventi di giornata intera
+    this.data.eventi.forEach(evento => {
+      if (evento.giornataIntera) {
+        evento.orarioInizio = '00:00';
+        evento.orarioFine = '23:59';
+      }
+    });
   }
+
 
   ngAfterViewInit(): void {
     const scrollTop = this.calcolaPosizioneOrario('08:00');
@@ -67,117 +76,70 @@ export class EventiDettaglioComponent implements OnInit, AfterViewInit {
   }
 
   aggiungiAlCalendario(tipo: string): void {
-    this.data.eventi.forEach(evento => {
-      const dataEvento = this.parseDateTime(evento.data, evento.orarioInizio);
-      const dataFineEvento = evento.orarioFine ? this.parseDateTime(evento.data, evento.orarioFine) : null;
-
-      if (isNaN(dataEvento.getTime()) || (dataFineEvento && isNaN(dataFineEvento.getTime()))) {
-        console.error('Data non valida:', dataEvento, dataFineEvento);
-        return;
-      }
-
-      const dataInizio = this.formatDate(dataEvento);
-      const dataFine = dataFineEvento ? this.formatDate(dataFineEvento) : '';
-
-      switch (tipo) {
-        case 'apple':
-          this.downloadICSFile(evento, dataInizio, dataFine);
-          break;
-        case 'google':
-          this.addToGoogleCalendar(evento, dataInizio, dataFine);
-          break;
-        case 'outlook':
-          this.addToOutlookCalendar(evento, dataInizio, dataFine);
-          break;
-        case 'yahoo':
-          this.addToYahooCalendar(evento, dataInizio, dataFine);
-          break;
-      }
+    const eventiDelGiorno = this.data.eventi.filter(evento => {
+      const eventDate = new Date(evento.data).toDateString();
+      const selectedDate = new Date(this.data.eventi[0].data).toDateString();
+      return eventDate === selectedDate;
     });
-  }
 
-  parseDateTime(data: string, orario?: string): Date {
-    return orario ? new Date(`${data}T${orario}:00`) : new Date(`${data}T00:00:00`);
-  }
+    console.log("Eventi del giorno selezionato:", eventiDelGiorno);
 
-  formatDate(date: Date): string {
-    return date.toISOString().replace(/-|:|\.\d{3}/g, '');
-  }
+    if (eventiDelGiorno.length === 0) {
+      console.warn('Nessun evento trovato per il giorno selezionato.');
+      return;
+    }
 
-  downloadICSFile(evento: Evento, dataInizio: string, dataFine: string): void {
-    const icsContent = `
-BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Your Organization//NONSGML v1.0//EN
-CALSCALE:GREGORIAN
-METHOD:PUBLISH
-BEGIN:VEVENT
-UID:${this.generateUID()}
-DTSTAMP:${this.formatDateToICS(new Date())}
-DTSTART:${dataInizio}
-${dataFine ? `DTEND:${dataFine}` : ''}
-SUMMARY:${evento.titolo}
-DESCRIPTION:${evento.descrizione}
-LOCATION:${evento.luogo}
-STATUS:CONFIRMED
-SEQUENCE:0
-END:VEVENT
-END:VCALENDAR
-    `.trim();
+    const allDayEvent = eventiDelGiorno.some(evento => evento.giornataIntera);
 
-    const blob = new Blob([icsContent], { type: 'text/calendar;charset=utf-8' });
-    const url = window.URL.createObjectURL(blob);
+    let dataInizio: string, dataFine: string;
 
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `${evento.titolo}.ics`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
+    if (allDayEvent) {
+      const dataEvento = new Date(eventiDelGiorno[0].data);
+      dataInizio = this.formatDate(dataEvento, true);
+      dataFine = this.formatDate(dataEvento, true);
+    } else {
+      const orarioInizioMin = eventiDelGiorno.reduce((min, evento) => {
+        const orarioInizio = new Date(`${evento.data}T${evento.orarioInizio || '00:00:00'}`).getTime();
+        return orarioInizio < min ? orarioInizio : min;
+      }, Infinity);
 
-  addToGoogleCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
-    const googleCalendarUrl = `https://calendar.google.com/calendar/r/eventedit?text=${encodeURIComponent(evento.titolo)}&dates=${dataInizio}/${dataFine}&details=${encodeURIComponent(evento.descrizione)}&location=${encodeURIComponent(evento.luogo)}`;
-    window.open(googleCalendarUrl, '_blank');
-  }
+      const orarioFineMax = eventiDelGiorno.reduce((max, evento) => {
+        const orarioFine = evento.orarioFine ? new Date(`${evento.data}T${evento.orarioFine}`).getTime() : new Date(`${evento.data}T23:59:59`).getTime();
+        return orarioFine > max ? orarioFine : max;
+      }, -Infinity);
 
-  addToOutlookCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
-    const outlookCalendarUrl = `https://outlook.live.com/owa/?path=/calendar/action/compose&startdt=${dataInizio}&enddt=${dataFine}&subject=${encodeURIComponent(evento.titolo)}&body=${encodeURIComponent(evento.descrizione)}&location=${encodeURIComponent(evento.luogo)}`;
-    window.open(outlookCalendarUrl, '_blank');
-  }
+      dataInizio = this.formatDate(new Date(orarioInizioMin));
+      dataFine = this.formatDate(new Date(orarioFineMax));
+    }
 
-  addToYahooCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
-    const yahooCalendarUrl = `https://calendar.yahoo.com/?v=60&view=d&type=20&title=${encodeURIComponent(evento.titolo)}&st=${dataInizio}&et=${dataFine}&desc=${encodeURIComponent(evento.descrizione)}&in_loc=${encodeURIComponent(evento.luogo)}`;
-    window.open(yahooCalendarUrl, '_blank');
-  }
+    console.log("Data inizio:", dataInizio);
+    console.log("Data fine:", dataFine);
+    console.log("Giornata intera:", allDayEvent);
 
-  formatDateToICS(date: Date): string {
-    return date.toISOString().replace(/-|:|\.\d{3}/g, '') + 'Z';
-  }
+    // Crea la descrizione aggregata
+    const descrizione = eventiDelGiorno
+      .map(evento => {
+        if (evento.giornataIntera) {
+          return `${evento.titolo} (Tutta la giornata)\n${evento.descrizione}\n${evento.luogo}`;
+        } else {
+          const orarioInizio = evento.orarioInizio ? evento.orarioInizio : 'TBD';
+          const orarioFine = evento.orarioFine ? evento.orarioFine : 'TBD';
+          return `${evento.titolo} (${orarioInizio} - ${orarioFine})\n${evento.descrizione}\n${evento.luogo}`;
+        }
+      })
+      .join('\n\n');
 
-  generateUID(): string {
-    return `${new Date().getTime()}@yourorganization.com`;
-  }
-}
+    console.log("Descrizione aggregata:", descrizione);
 
-
-
-  //codice per creare un evento unico con ora inizio del primo evento della giornata e ora fine come ora fine dell'ultimo evento
-    /*
-    aggiungiAlCalendario(tipo: string): void {
-    const eventoInizio = this.data.eventi[0];
-    const eventoFine = this.data.eventi[this.data.eventi.length - 1];
-
-    const dataInizio = this.formatDate(new Date(`${this.data.anno}-${this.formatMonth()}-${this.pad(this.data.giorno)}T${eventoInizio.orarioInizio}`));
-    const dataFine = eventoFine.orarioFine ? this.formatDate(new Date(`${this.data.anno}-${this.formatMonth()}-${this.pad(this.data.giorno)}T${eventoFine.orarioFine}`)) : '';
-
-    const descrizione = this.data.eventi.map(e => `${e.orarioInizio} - ${e.orarioFine}: ${e.titolo}\n${e.descrizione}`).join('\n\n');
-
-    const eventoUnico = {
-      titolo: 'Eventi del Giorno',
-      descrizione: descrizione,
-      luogo: eventoInizio.luogo
+    const eventoUnico: Evento = {
+      titolo: 'Eventi di Parola Giovane',
+      descrizione: descrizione.trim(),
+      luogo: eventiDelGiorno[0].luogo,
+      data: eventiDelGiorno[0].data,
+      giornataIntera: allDayEvent
     };
+
+    console.log("Evento unificato:", eventoUnico);
 
     switch (tipo) {
       case 'apple':
@@ -192,6 +154,75 @@ END:VCALENDAR
       case 'yahoo':
         this.addToYahooCalendar(eventoUnico, dataInizio, dataFine);
         break;
+      default:
+        console.error('Tipo di calendario non riconosciuto:', tipo);
     }
   }
-  */
+
+  formatDate(date: Date, allDay: boolean = false): string {
+    if (allDay) {
+      return date.toISOString().split('T')[0].replace(/-/g, '');
+    } else {
+      return date.toISOString().replace(/-|:|\.\d{3}/g, '');
+    }
+  }
+
+  downloadICSFile(evento: Evento, dataInizio: string, dataFine: string): void {
+    const icsContent = `
+  BEGIN:VCALENDAR
+  VERSION:2.0
+  PRODID:-//Your Organization//NONSGML v1.0//EN
+  CALSCALE:GREGORIAN
+  METHOD:PUBLISH
+  BEGIN:VEVENT
+  UID:${this.generateUID()}
+  DTSTAMP:${this.formatDateToICS(new Date())}
+  ${evento.giornataIntera ? `DTSTART;VALUE=DATE:${dataInizio.split('T')[0]}` : `DTSTART:${dataInizio}`}
+  ${evento.giornataIntera ? '' : `DTEND:${dataFine}`}
+  SUMMARY:${evento.titolo}
+  DESCRIPTION:${evento.descrizione}
+  LOCATION:${evento.luogo}
+  STATUS:CONFIRMED
+  SEQUENCE:0
+  END:VEVENT
+  END:VCALENDAR
+    `.trim();
+
+    const blob = new Blob([icsContent], { type: 'text/calendar;charset=utf-8' });
+    const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `${evento.titolo}.ics`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  addToGoogleCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
+    const googleCalendarUrl = `https://calendar.google.com/calendar/r/eventedit?text=${encodeURIComponent(evento.titolo)}&dates=${evento.giornataIntera ? `${dataInizio.split('T')[0]}/${dataInizio.split('T')[0]}` : `${dataInizio}/${dataFine}`}&details=${encodeURIComponent(evento.descrizione)}&location=${encodeURIComponent(evento.luogo)}`;
+    window.open(googleCalendarUrl, '_blank');
+  }
+
+  addToOutlookCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
+    const outlookCalendarUrl = `https://outlook.live.com/owa/?path=/calendar/action/compose&startdt=${evento.giornataIntera ? `${dataInizio.split('T')[0]}` : `${dataInizio}`}&enddt=${evento.giornataIntera ? '' : `${dataFine}`}&subject=${encodeURIComponent(evento.titolo)}&body=${encodeURIComponent(evento.descrizione)}&location=${encodeURIComponent(evento.luogo)}`;
+    window.open(outlookCalendarUrl, '_blank');
+  }
+
+  addToYahooCalendar(evento: Evento, dataInizio: string, dataFine: string): void {
+    const yahooCalendarUrl = `https://calendar.yahoo.com/?v=60&view=d&type=20&title=${encodeURIComponent(evento.titolo)}&st=${evento.giornataIntera ? `${dataInizio.split('T')[0]}` : `${dataInizio}`}&et=${evento.giornataIntera ? '' : `${dataFine}`}&desc=${encodeURIComponent(evento.descrizione)}&in_loc=${encodeURIComponent(evento.luogo)}`;
+    window.open(yahooCalendarUrl, '_blank');
+  }
+
+  parseDateTime(data: string, orario?: string): Date {
+    return orario ? new Date(`${data}T${orario}:00`) : new Date(`${data}T00:00:00`);
+  }
+
+  formatDateToICS(date: Date): string {
+    return date.toISOString().replace(/-|:|\.\d{3}/g, '') + 'Z';
+  }
+
+  generateUID(): string {
+    return `${new Date().getTime()}@yourorganization.com`;
+  }
+}
